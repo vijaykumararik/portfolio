85432584592e008ac7704b6aade3058ffa24d363
vijaykumararik <vijaykumararikatti@gmail.com>
2023-06-02 11:36:03 +0530

    done

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -17,9 +17,10 @@ const Experience = () => {
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
         <ResumeCard
             title=" Software Engineer"
-            subTitle="Nirvanasoft tech - (2021 - Present)"
+            subTitle="Complted java full stack cource in jspiders"
             result="Bangalore"
-            des="I am in UI developement using REACTJS tool and i worked in hospital management project  "
+            des="I have skills like HTML,CSS JAVASCRIPT ,REACTJS,JAJA,HIBERNATE,SPRINGBOOT,
+            and have complete knowledge about the tools like GIT,GITHUB,POSTMAN,VSCODE,ECLIPSE  "
           />
           <ResumeCard
             title="Mathematics subject matter expert"
